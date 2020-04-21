beef234d3037ad154a875e06139a4d6d1960ce40
ravi6706 <53494249+ravi6706@users.noreply.github.com>
2020-04-21 16:18:58 +0530

    commit

---
+++ spring-mvc-rest/src/main/java/com/lpu/entity/Student.java
@@ -0,0 +1,41 @@
+package com.lpu.entity;
+
+public class Student {
+
+	private int studentID;
+	private String firstName;
+	private String lastName;
+	
+	public Student() {
+		
+	}
+
+
+
+
+	public Student(int studentID, String firstName, String lastName) {
+		super();
+		this.studentID = studentID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
---
+++ spring-mvc-rest/src/main/java/com/lpu/rest/StudentRestController.java
@@ -0,0 +1,46 @@
+package com.lpu.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lpu.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+	// define endpoint for "/students" - return list of students
+	List<Student> theStudents=null;
+	@PostConstruct
+	public void init()
+	{
+		theStudents = new ArrayList<Student>();
+		
+		theStudents.add(new Student(0,"Poornima", "Patel"));
+		theStudents.add(new Student(1,"Mario", "Rossi"));
+		theStudents.add(new Student(2,"Mary", "Smith"));		
+		
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+
+		
+		return theStudents;
+	}
+	
+	@GetMapping("/students/{studentID}")
+	public Student getStudentByID(@PathVariable int studentID)
+	{
+		return theStudents.get(studentID);
+	}
+	
+	
+}
