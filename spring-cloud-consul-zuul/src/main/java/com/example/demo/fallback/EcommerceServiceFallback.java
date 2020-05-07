1ad607a02ac9b1bc41a255bae5681df633c01edc
ravi6706 <53494249+ravi6706@users.noreply.github.com>
2020-05-07 11:37:12 +0530

    commit

---
+++ spring-cloud-consul-zuul/src/main/java/com/example/demo/fallback/EcommerceServiceFallback.java
@@ -0,0 +1,29 @@
+package com.example.demo.fallback;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+
+@Component
+class EcommerceServiceFallback implements FallbackProvider {
+
+    private static final String DEFAULT_MESSAGE = "Ecommerce service is not available.";
+
+    @Override
+    public String getRoute() {
+        return "ecommerce-service";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        if (cause instanceof HystrixTimeoutException) {
+            return new GatewayClientResponse(HttpStatus.GATEWAY_TIMEOUT, DEFAULT_MESSAGE);
+        } else {
+            return new GatewayClientResponse(HttpStatus.INTERNAL_SERVER_ERROR, DEFAULT_MESSAGE);
+        }
+    }
+
+}
